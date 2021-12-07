e9bc20c64db1ffa481a51219e515314903e2e870
Aylin-ai <69709234+Aylin-ai@users.noreply.github.com>
2021-12-07 12:32:41 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,70 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Задание_4
+namespace Задание_3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Random rand = new Random();
-            int n = 4;
-            int m = 8;
-            int max = 0;
-            int min = 0;
-            int[,] arrA = new int[n, m];
-            int[] maxarr = new int[n];
-            for (int i = 0; i < n; i++) 
-            {
-                for (int j = 0; j < m; j++) 
-                {
-                    arrA[i, j] = rand.Next(-19, 31);
-                }
-            }
-
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < m; j++)
-                {
-                    Console.Write(arrA[i, j]);
-                    Console.Write(' ');
-                }
-                Console.WriteLine();
-            }
-
-            Console.WriteLine("Максимальные элементы");
-            for (int i = 0; i < n; i++) 
-            {
-                max = arrA[i, 0];
-                for (int j = 0; j < m; j++) 
-                {
-                    if (arrA[i, j] > max)
-                    {
-                        max = arrA[i, j];
-                    }
-                }
-                maxarr[i] = max;
-            }
-            for (int k = 0; k < n; k++) 
-            {
-                Console.Write(maxarr[k]);
-                Console.Write(" ");
-            }
-            Console.WriteLine();
-            Console.WriteLine("Минимальные элементы");
-            for (int j = 0; j < m; j++)
-            {
-                min = arrA[0, j];
-                for (int i = 0; i < n; i++)
-                {
-                    if (arrA[i, j] < min)
-                    {
-                        min = arrA[i, j];
-                    }
-                }
-                Console.Write(min);
-                Console.Write(' ');
-            }
+            string k;
+            char n1;
+            char n2;
+            int n = 39;
+            string N = Convert.ToString(n);
+            n1 = N[0];
+            n2 = N[1];
+            k = Convert.ToString(N[1]) + Convert.ToString(N[0]);
+            n = Convert.ToInt32(k);
+            Console.WriteLine(n);
             Console.ReadKey();
         }
     }
